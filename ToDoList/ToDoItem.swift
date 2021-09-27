798fca7044add09a87d6baa5d990fd99887f8102
Connor Goodman <goodmaco@bc.edu>
2021-09-27 15:34:35 -0400

    Finished Week 5 Work - ToDoList (5.1 - 5.9)

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Connor Goodman on 9/26/21.
+//
+
+import Foundation
+
+struct ToDoItem: Codable{
+    var name: String
+    var date: Date
+    var notes: String
+    var reminderSet: Bool
+}
